fd4cba56fc05d6127a729f75d6eb684e8c2aee68
bacilo <i@pedropaf.com>
2023-09-24 15:36:47 +0200

    added AI generated tests for encodeBytes

--- tests/zpass3.spec.js
+++ tests/zpass3.spec.js
@@ -1,4 +1,4 @@
-const { checkComplexity } = require("./libs/zpass3");
+const { checkComplexity, encodeBytes } = require("./libs/zpass3");
 
 test("has number", () => {
     expect(checkComplexity('^n3msCR#7sWD!w')).toBe(true);
@@ -27,3 +27,43 @@ test("has symbol", () => {
 test("has symbol", () => {
     expect(checkComplexity('n3msCR7sWDw')).toBe(false);
 });
+
+// Test encoding a blank array
+test('encode empty array', () => {
+    const input = [];
+    const expected = '';
+    expect(encodeBytes(input)).toBe(expected);
+});
+
+// Test encoding a sample array
+test('encode sample array', () => {
+    const input = [0x41, 0x42, 0xff];
+    const expected = 'AB??FF';
+    expect(encodeBytes(input)).toBe(expected);
+});
+
+// Test decoding encoded string back to original bytes
+// test('decode to original bytes', () => {
+//     const encoded = 'AB??FF';
+//     const expected = [0x41, 0x42, 0xff];
+//     expect(decodeBytes(encoded   )).toEqual(expected);
+// });
+
+// Test handling max byte value
+// test('handle max byte', () => {
+//     const input = [0xff];
+//     const expected = '?G';
+//     expect(encodeBytes(input)).toBe(expected);
+// });
+//
+// // Test handling min byte value
+// test('handle min byte', () => {
+//     const input = [0x00];
+//     const expected = 'aG';
+//     expect(encodeBytes(input)).toBe(expected);
+// });
+//
+// // Test invalid input
+// test('invalid input', () => {
+//     expect(() => encodeBytes('invalid')).toThrow();
+// });
